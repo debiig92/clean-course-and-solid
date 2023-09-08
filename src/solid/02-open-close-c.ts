b920a2201e04c9f25f16f7c7dcc78ceedbd9ffbe
Débora Melissa Guardado Reinoza <debora_guardado@unicomer.com>
2023-09-08 15:32:57 -0600

    :art: open close refactor removing axios

--- src/solid/02-open-close-c.ts
+++ src/solid/02-open-close-c.ts
@@ -1,8 +1,14 @@
 // Hay que agregar la dependencia de axios ```yarn add axios```
-import axios from 'axios';
+//import axios from 'axios';
 
 export class HttpClient {
+    // async get(url: string) {
+    //     return await axios.get(url);
+    // }
+
     async get(url: string) {
-        return await axios.get(url);
+        const resp = await fetch(url);
+        const data = await resp.json();
+        return {data};
     }
 }
